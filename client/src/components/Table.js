359a8f4c706853b3a2601048c80b89a9fbefa3c6
Jaira Arcilla <arcilla.jaira@gmail.com>
2021-02-27 04:31:05 -0800

    add table

---
+++ client/src/components/Table.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TableData from "./TableData";
+
+function Table(props) {
+    return (
+        <table className="table table-striped table-hover table-fluid">
+            <thead>
+                <tr>
+                    <th scope="col">Picture</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                </tr>
+            </thead>
+            <TableData results={props.results} key={props.phone}
+            />
+        </table>
+    )
+}
+export default Table;
